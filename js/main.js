1b29043151bea3d753152373adeec6221cf56dba
Michael Johnson <michael@theplasticfactory.us>
2012-11-09 15:33:49 -0500

    Initial commit

---
+++ js/main.js
@@ -0,0 +1,24 @@
+/*
+ * Call some of our helper functions - plugins.js
+ * https://github.com/h5bp/mobile-boilerplate/wiki/JavaScript-Helper
+ */
+
+MBP.scaleFix();
+MBP.hideUrlBarOnLoad();
+//new MBP.autogrow(document.getElementById('myTextarea'), 14); // 14 - line height
+
+(function($){
+
+$(function() {
+
+
+
+}); // End document.load (DOM loaded)
+
+$(window).load(function() {
+
+
+
+}); // End window.load (Everything loaded)
+
+})(this.jQuery); // End jquery namespace
