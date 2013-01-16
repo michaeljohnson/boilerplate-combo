0f2327e64eee3357a87989fffcf8d07617c4008b
Michael Johnson <michael@theplasticfactory.us>
2013-01-15 22:19:50 -0500

    More overhauling.

--- public/js/main.js
+++ public/js/main.js
@@ -7,6 +7,10 @@ MBP.scaleFix();
 MBP.hideUrlBarOnLoad();
 //new MBP.autogrow(document.getElementById('myTextarea'), 14); // 14 - line height
 
+/**
+ *  Call our functions
+ */
+
 (function($){
 
 $(function() {
@@ -20,5 +24,19 @@ $(window).load(function() {
 
 
 }); // End window.load (Everything loaded)
+
+function windowScroll() {
+    if( $(window).scrollTop() > 163 ) {
+        $('body').addClass('toolbar');
+        $('header').not('.toolbar').hide().addClass('toolbar').fadeIn( 400 );
+        $("#top-link").fadeIn();
+    } else if( $(window).scrollTop() < 32 ) {
+        $('body.toolbar').removeClass('toolbar');
+        $('header.toolbar').removeClass('toolbar').show();
+        $("#top-link").fadeOut();
+    } else {
+        $('body.toolbar').removeClass('toolbar');
+    }
+};
 
 })(this.jQuery); // End jquery namespace
